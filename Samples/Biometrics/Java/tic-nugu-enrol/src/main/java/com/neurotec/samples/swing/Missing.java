228c73f9d7a95134e632ad807c1e7718d4cc14e3
ouedraogo Aristide ouedraogo <ouedr@CENI.ceni.local>
2024-10-21 17:28:10 +0000

    gestion des doigts manquants

--- Samples/Biometrics/Java/tic-nugu-enrol/src/main/java/com/neurotec/samples/swing/Missing.java
+++ Samples/Biometrics/Java/tic-nugu-enrol/src/main/java/com/neurotec/samples/swing/Missing.java
@@ -2,6 +2,7 @@ package com.neurotec.samples.swing;
 
 public class Missing {
     private int missingSize;
+    private int nbTemplate;
 
     public int getMissingSize() {
         return missingSize;
@@ -10,10 +11,19 @@ public class Missing {
     public void setMissingSize(int missingSize) {
         this.missingSize = missingSize;
     }
-    public  boolean activate(int nbTemplate){
+
+
+    public int getNbTemplate() {
+        return nbTemplate;
+    }
+
+    public void setNbTemplate(int nbt) {
+        this.nbTemplate = nbt;
+    }
+    public  boolean activate(){
        int  nbFinger=10;
        boolean ok=false;
-        if((nbFinger-this.missingSize)== nbTemplate){
+        if((nbFinger-this.missingSize)== this.nbTemplate){
             ok=  true;
         }
         return  ok;
--- Samples/Biometrics/Java/tic-nugu-enrol/src/main/java/com/neurotec/samples/swing/controls/SlapsPanel.java
+++ Samples/Biometrics/Java/tic-nugu-enrol/src/main/java/com/neurotec/samples/swing/controls/SlapsPanel.java
@@ -3,12 +3,25 @@ package com.neurotec.samples.swing.controls;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.Random;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 import com.neurotec.biometrics.NFPosition;
+import com.neurotec.biometrics.NSubject;
 import com.neurotec.biometrics.swing.NFingerView;
+import com.neurotec.samples.Utilities;
+import com.neurotec.samples.swing.FingerCaptureFrame;
 import com.neurotec.samples.swing.GridBagUtils;
+import com.neurotec.samples.swing.Missing;
+import com.neurotec.samples.swing.Mongo;
 
 public final class SlapsPanel extends JPanel {
 
@@ -24,6 +37,9 @@ public final class SlapsPanel extends JPanel {
 	private NFingerView nfvLeftFour;
 	private NFingerView nfvRightFour;
 	private NFingerView nfvThumbs;
+
+	private String directory;
+
 
 	// ==============================================
 	// Public constructor
@@ -80,10 +96,12 @@ public final class SlapsPanel extends JPanel {
 
 		JButton saveAllButton = new JButton("Enregistrer toutes les empreintes");
 		saveAllButton.setPreferredSize(new Dimension(50, 40));  // Largeur 150, Hauteur 40
+	//	saveAllButton.setBackground(Color.LIGHT_GRAY);
 
+
+	//	saveAllButton.setForeground(Color.WHITE);  // Texte en blanc
+		saveAllButton.setEnabled(true);
 		saveAllButton.setBackground(new Color(0, 128, 255));  // Bleu clair
-		saveAllButton.setForeground(Color.WHITE);  // Texte en blanc
-
 		// Créer des contraintes spécifiques pour le bouton
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.gridx = 1;  // Colonne
@@ -105,7 +123,169 @@ public final class SlapsPanel extends JPanel {
 		});
 		//gridBagUtils.addToGridBagLayout(1, 2, this, saveAllButton);
 	}
-	private void saveAllFingerprints() {}
+
+	public String getDirectory(){
+		return  this.directory;
+	}
+
+	public  void setDirectory(String d){
+		this.directory=d;
+	}
+	private void saveAllFingerprints() {
+
+
+		Missing mss=new Missing();
+		FingerCaptureFrame fgf =new FingerCaptureFrame(new Frame());
+
+
+
+
+        if(fgf.getSubject() !=null && fgf.getSubject().getTemplate().getFingers()!=null){
+
+
+			if((10-fgf.getSubject().getMissingFingers().size())==fgf.getSubject().getTemplate().getFingers().getRecords().size()){
+				Mongo mg=new Mongo();
+				setDirectory(createRandomDirectory());
+
+
+				fgf.getSubject().getTemplate().getFingers().getRecords().forEach(element -> {
+					System.out.println("<=======================Nombre de munities par doigt="+element.getMinutiae().size());
+
+					for (int i=0;i<element.getMinutiae().size();i++){
+						mg.insertMunitieToMongoDB(element.getMinutiae().get(i).x,element.getMinutiae().get(i).y,element.getPosition().toString());
+
+					}
+
+				});
+
+				System.out.println("<=======================Nombre de doigt="+fgf.getSubject().getFingers().size());
+
+
+				for (int i=0;i<fgf.getSubject().getFingers().size();i++){
+					System.out.println("<=======================Nom de doigt="+fgf.getSubject().getFingers().get(i).getPosition().toString());
+
+					if(fgf.getSubject().getFingers().get(i).getBinarizedImage(true)!=null){
+						try {
+							convertBase64ToPNG(convertToBase64(fgf.getSubject().getFingers().get(i).getImage().toImage(),"png"),fgf.getSubject().getFingers().get(i).getPosition().toString());
+						} catch (IOException e) {
+							throw new RuntimeException(e);
+						}
+						mg.insertImageToMongoDB(mg.convertToByteArray(mg.convert(convertToBase64(fgf.getSubject().getFingers().get(i).getImage().toImage(),"png"))),fgf.getSubject().getFingers().get(i).getPosition().toString());
+
+					}
+				}
+				Utilities.showWarning(this, "Sauvegarde effectuée avec succès");
+
+			//	System.out.println( "Nombre de doigts manquants dans save all: "+fgf.getSubject().getMissingFingers().size());
+
+			//	System.out.println( "Nombre de doigts enregistrés  dans save all: "+fgf.getSubject().getTemplate().getFingers().getRecords().size());
+
+
+
+			}
+			else {
+				Utilities.showWarning(this, "Veuillez prendre les empreintes de tous les doigts disponibles");
+
+			}
+
+		}
+		else{
+			Utilities.showWarning(this, "Veuillez prendre les empreintes de tous les doigts disponibles");
+
+
+		}
+
+
+
+
+
+	}
+
+	public  void convertBase64ToPNG(String base64String, String fileName) throws IOException {
+		// Retirer le préfixe si présent (par exemple, "data:image/png;base64,")
+		if (base64String.startsWith("data:image/png;base64,")) {
+			base64String = base64String.substring("data:image/png;base64,".length());
+		}
+
+		String main;
+		String nomFichier="";
+		String x1 =fileName.split("_")[0];
+		if(x1.equals("LEFT")){
+			main="Gauche"+".png";
+		}
+		else {
+			main="Droite"+".png";
+		}
+
+		String dg=fileName.split("_")[1];
+		if(dg.equals("LITTLE")){
+			nomFichier="Auriculaire_"+main;
+		}
+		else if(dg.equals("RING")){
+			nomFichier="Annulaire_"+main;
+		}
+		else if(dg.equals("MIDDLE")){
+			nomFichier="Majeure_"+main;
+		}
+		else if(dg.equals("INDEX")){
+			nomFichier="Index_"+main;
+		}
+		else if(dg.equals("THUMB")){
+			nomFichier="Pouce_"+main;
+		}
+
+		// Décoder la chaîne Base64
+		byte[] imageBytes = Base64.getDecoder().decode(base64String);
+
+		File dir = new File(getDirectory());
+		// Écrire les octets dans un fichier
+		/*try (FileOutputStream fos = new FileOutputStream(getDirectory()+fileName)) {
+			fos.write(imageBytes);
+		}*/
+		try (FileOutputStream fos = new FileOutputStream(new File(dir, nomFichier))) {
+			fos.write(imageBytes);
+		}
+	}
+
+	public  String convertToBase64(Image img, String formatName) {
+		// Convert Image to BufferedImage
+		BufferedImage bufferedImage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+		bufferedImage.getGraphics().drawImage(img, 0, 0, null);
+
+		// Convert BufferedImage to Base64
+		try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+			ImageIO.write(bufferedImage, formatName, baos);
+			byte[] imageBytes = baos.toByteArray();
+			return Base64.getEncoder().encodeToString(imageBytes);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+
+	public static String createRandomDirectory() {
+		// Générer un Long aléatoire
+		Random random = new Random();
+		long randomLong = Math.abs(random.nextLong());
+
+		// Créer le nom du dossier
+		String directoryName = String.valueOf(randomLong);
+
+		// Créer le dossier
+		File directory = new File(directoryName);
+		if (!directory.exists()) {
+			if (directory.mkdir()) {
+				System.out.println("Dossier créé : " + directoryName);
+			} else {
+				System.out.println("Échec de la création du dossier : " + directoryName);
+			}
+		} else {
+			System.out.println("Le dossier existe déjà : " + directoryName);
+		}
+
+		return directoryName; // Retourne le nom du dossier
+	}
 	// ==============================================
 	// Public methods
 	// ==============================================
